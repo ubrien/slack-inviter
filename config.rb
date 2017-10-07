342686dfad57bdc6896e33e3970c64f8ba75ea42
hassan uriostegui <hassanvfx@gmail.com>
2017-10-07 14:03:51 -0700

    add correct token

--- config.rb
+++ config.rb
@@ -2,5 +2,5 @@ class Config
   SLACK_TEAM_NAME = "Social Video Gurus"
   SLACK_TEAM_URL = "https://socialvideopros.slack.com"
   # generate your API token at https://api.slack.com/
-  SLACK_TEAM_TOKEN = "xoxp-253986011879-253784987398-253024316498-d6f45aaf28d202a8a6b9fe88a58f12d7"
+  SLACK_TEAM_TOKEN = "xoxs-253986011879-253784987398-252382388353-eb055c9f47"
 end
