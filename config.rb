7461f380616d67c146b6e29a970162b36d53978b
hassan uriostegui <hassanvfx@gmail.com>
2017-10-07 13:44:57 -0700

    fxies

--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 class Config
-  SLACK_TEAM_NAME = "Ruby Web Devs"
-  SLACK_TEAM_URL = "https://rubywebdev.slack.com"
+  SLACK_TEAM_NAME = "Social Video Gurus"
+  SLACK_TEAM_URL = "https://socialvideopros.slack.com"
   # generate your API token at https://api.slack.com/
-  SLACK_TEAM_TOKEN = ENV["SLACK_TEAM_RUBYWEBDEV_API_TOKEN"]
+  SLACK_TEAM_TOKEN = "xoxp-253986011879-253784987398-253024316498-d6f45aaf28d202a8a6b9fe88a58f12d7"
 end
